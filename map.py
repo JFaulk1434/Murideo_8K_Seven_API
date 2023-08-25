0be1c8b5918357e7c24c8ebf3c9c582253a45afd
JFaulk1434 <j.faulk@me.com>
2023-08-25 10:59:04 -0400

    first

---
+++ map.py
@@ -0,0 +1,303 @@
+Murideo_WebUI = {  # Master Dictionary for WebUI of Murideo 8K Seven Genderator
+    "video_generator": {
+        "timing": {
+            "function": "sendsingle",  # Function needed to send the command ex: SENDSINGLE||97,110
+            "category_type": 97,  # ID For the category in this case it's 97=Timing
+            "8k": {
+                "7680x4320@30": {
+                    "id": 110,  # command to be called for this resolution
+                    "h_rez": 7680,  # Horizontal Pixels
+                    "v_rez": 4320,  # Vertical Pixels
+                    "refresh": 30,  # Refresh Rate
+                    "tag": "7680x4320 30Hz",  # The actual Button text
+                },
+                "7680x4320@29.97": {
+                    "id": 111,  # command to be called for this resolution
+                    "h_rez": 7680,  # Horizontal Pixels
+                    "v_rez": 4320,  # Vertical Pixels
+                    "refresh": 29.97,  # Refresh Rate
+                    "tag": "7680x4320 29.97Hz",  # The actual Button text
+                },
+                "7680x4320@25": {
+                    "id": 112,  # command to be called for this resolution
+                    "h_rez": 7680,  # Horizontal Pixels
+                    "v_rez": 4320,  # Vertical Pixels
+                    "refresh": 25,  # Refresh Rate
+                    "tag": "7680x4320 25Hz",  # The actual Button text
+                },
+                "7680x4320@24": {
+                    "id": 113,  # command to be called for this resolution
+                    "h_rez": 7680,  # Horizontal Pixels
+                    "v_rez": 4320,  # Vertical Pixels
+                    "refresh": 24,  # Refresh Rate
+                    "tag": "7680x4320 24Hz",  # The actual Button text
+                },
+                "7680x4320@23.98": {
+                    "id": 114,  # command to be called for this resolution
+                    "h_rez": 7680,  # Horizontal Pixels
+                    "v_rez": 4320,  # Vertical Pixels
+                    "refresh": 23.98,  # Refresh Rate
+                    "tag": "7680x4320 23.98Hz",  # The actual Button text
+                },
+                "7680x4320@60": {
+                    "id": 115,  # command to be called for this resolution
+                    "h_rez": 7680,  # Horizontal Pixels
+                    "v_rez": 4320,  # Vertical Pixels
+                    "refresh": 60,  # Refresh Rate
+                    "tag": "7680x4320 60Hz",  # The actual Button text
+                },
+                "7680x4320@59.94": {
+                    "id": 116,  # command to be called for this resolution
+                    "h_rez": 7680,  # Horizontal Pixels
+                    "v_rez": 4320,  # Vertical Pixels
+                    "refresh": 59.94,  # Refresh Rate
+                    "tag": "7680x4320 59.94Hz",  # The actual Button text
+                },
+                "7680x4320@50": {
+                    "id": 117,  # command to be called for this resolution
+                    "h_rez": 7680,  # Horizontal Pixels
+                    "v_rez": 4320,  # Vertical Pixels
+                    "refresh": 50,  # Refresh Rate
+                    "tag": "7680x4320 50Hz",  # The actual Button text
+                },
+                "7680x4320@48": {
+                    "id": 118,  # command to be called for this resolution
+                    "h_rez": 7680,  # Horizontal Pixels
+                    "v_rez": 4320,  # Vertical Pixels
+                    "refresh": 48,  # Refresh Rate
+                    "tag": "7680x4320 48Hz",  # The actual Button text
+                },
+                "7680x4320@47.95": {
+                    "id": 119,  # command to be called for this resolution
+                    "h_rez": 7680,  # Horizontal Pixels
+                    "v_rez": 4320,  # Vertical Pixels
+                    "refresh": 47.95,  # Refresh Rate
+                    "tag": "7680x4320 47.95Hz",  # The actual Button text
+                },
+            },
+            "uhd": {
+                "3840x2160@30": {
+                    "id": 28,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 30,  # Refresh Rate
+                    "tag": "3840x2160 30Hz",  # The actual Button text
+                },
+                "3840x2160@29.97": {
+                    "id": 29,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 29.97,  # Refresh Rate
+                    "tag": "3840x2160 29.97Hz",  # The actual Button text
+                },
+                "3840x2160@25": {
+                    "id": 30,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 25,  # Refresh Rate
+                    "tag": "3840x2160 25Hz",  # The actual Button text
+                },
+                "3840x2160@24": {
+                    "id": 31,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 24,  # Refresh Rate
+                    "tag": "3840x2160 24Hz",  # The actual Button text
+                },
+                "3840x2160@23.98": {
+                    "id": 32,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 23.98,  # Refresh Rate
+                    "tag": "3840x2160 23.98Hz",  # The actual Button text
+                },
+                "3840x2160@60": {
+                    "id": 34,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 60,  # Refresh Rate
+                    "tag": "3840x2160 60Hz",  # The actual Button text
+                },
+                "3840x2160@59.94": {
+                    "id": 35,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 59.94,  # Refresh Rate
+                    "tag": "3840x2160 59.94Hz",  # The actual Button text
+                },
+                "3840x2160@50": {
+                    "id": 36,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 50,  # Refresh Rate
+                    "tag": "3840x2160 50Hz",  # The actual Button text
+                },
+                "3840x2160@48": {
+                    "id": 103,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 48,  # Refresh Rate
+                    "tag": "3840x2160 48Hz",  # The actual Button text
+                },
+                "3840x2160@47.95": {
+                    "id": 104,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 47.95,  # Refresh Rate
+                    "tag": "3840x2160 47.95Hz",  # The actual Button text
+                },
+                "3840x2160@100": {
+                    "id": 107,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 100,  # Refresh Rate
+                    "tag": "3840x2160 100Hz",  # The actual Button text
+                },
+                "3840x2160@120": {
+                    "id": 108,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 120,  # Refresh Rate
+                    "tag": "3840x2160 120Hz",  # The actual Button text
+                },
+                "3840x2160@119.88": {
+                    "id": 109,  # command to be called for this resolution
+                    "h_rez": 3840,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 119.88,  # Refresh Rate
+                    "tag": "3840x2160 119.88Hz",  # The actual Button text
+                },
+            },
+            "4k-dci": {
+                "4096x2160@30": {
+                    "id": 53,  # command to be called for this resolution
+                    "h_rez": 4096,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 30,  # Refresh Rate
+                    "tag": "4096x2160 30Hz",  # The actual Button text
+                },
+                "4096x2160@29.97": {
+                    "id": 54,  # command to be called for this resolution
+                    "h_rez": 4096,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 29.97,  # Refresh Rate
+                    "tag": "4096x2160 29.97Hz",  # The actual Button text
+                },
+                "4096x2160@25": {
+                    "id": 55,  # command to be called for this resolution
+                    "h_rez": 4096,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 25,  # Refresh Rate
+                    "tag": "4096x2160 25Hz",  # The actual Button text
+                },
+                "4096x2160@24": {
+                    "id": 44,  # command to be called for this resolution
+                    "h_rez": 4096,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 24,  # Refresh Rate
+                    "tag": "4096x2160 24Hz",  # The actual Button text
+                },
+                "4096x2160@23.976": {
+                    "id": 56,  # command to be called for this resolution
+                    "h_rez": 4096,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 23.976,  # Refresh Rate
+                    "tag": "4096x2160 23.976Hz",  # The actual Button text
+                },
+                "4096x2160@60": {
+                    "id": 57,  # command to be called for this resolution
+                    "h_rez": 4096,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 60,  # Refresh Rate
+                    "tag": "4096x2160 60Hz",  # The actual Button text
+                },
+                "4096x2160@59.94": {
+                    "id": 58,  # command to be called for this resolution
+                    "h_rez": 4096,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 59.94,  # Refresh Rate
+                    "tag": "4096x2160 59.94Hz",  # The actual Button text
+                },
+                "4096x2160@50": {
+                    "id": 59,  # command to be called for this resolution
+                    "h_rez": 4096,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 50,  # Refresh Rate
+                    "tag": "4096x2160 50Hz",  # The actual Button text
+                },
+                "4096x2160@48": {
+                    "id": 105,  # command to be called for this resolution
+                    "h_rez": 4096,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 48,  # Refresh Rate
+                    "tag": "4096x2160 48Hz",  # The actual Button text
+                },
+                "4096x2160@47.95": {
+                    "id": 106,  # command to be called for this resolution
+                    "h_rez": 4096,  # Horizontal Pixels
+                    "v_rez": 2160,  # Vertical Pixels
+                    "refresh": 47.95,  # Refresh Rate
+                    "tag": "4096x2160 47.95Hz",  # The actual Button text
+                },
+            },
+            "2k-dci": {
+                "2048x1080@30": {
+                    "id": 73,  # command to be called for this resolution
+                    "h_rez": 2048,  # Horizontal Pixels
+                    "v_rez": 1080,  # Vertical Pixels
+                    "refresh": 30,  # Refresh Rate
+                    "tag": "2048x1080 30Hz",  # The actual Button text
+                },
+                "2048x1080@29.97": {
+                    "id": 74,  # command to be called for this resolution
+                    "h_rez": 2048,  # Horizontal Pixels
+                    "v_rez": 1080,  # Vertical Pixels
+                    "refresh": 29.97,  # Refresh Rate
+                    "tag": "2048x1080 29.97Hz",  # The actual Button text
+                },
+                "2048x1080@25": {
+                    "id": 75,  # command to be called for this resolution
+                    "h_rez": 2048,  # Horizontal Pixels
+                    "v_rez": 1080,  # Vertical Pixels
+                    "refresh": 25,  # Refresh Rate
+                    "tag": "2048x1080 25Hz",  # The actual Button text
+                },
+                "2048x1080@24": {
+                    "id": 76,  # command to be called for this resolution
+                    "h_rez": 2048,  # Horizontal Pixels
+                    "v_rez": 1080,  # Vertical Pixels
+                    "refresh": 24,  # Refresh Rate
+                    "tag": "2048x1080 24Hz",  # The actual Button text
+                },
+                "2048x1080@23.976": {
+                    "id": 77,  # command to be called for this resolution
+                    "h_rez": 2048,  # Horizontal Pixels
+                    "v_rez": 1080,  # Vertical Pixels
+                    "refresh": 23.976,  # Refresh Rate
+                    "tag": "2048x1080 23.976Hz",  # The actual Button text
+                },
+                "2048x1080@60": {
+                    "id": 78,  # command to be called for this resolution
+                    "h_rez": 2048,  # Horizontal Pixels
+                    "v_rez": 1080,  # Vertical Pixels
+                    "refresh": 60,  # Refresh Rate
+                    "tag": "2048x1080 60Hz",  # The actual Button text
+                },
+                "2048x1080@59.94": {
+                    "id": 79,  # command to be called for this resolution
+                    "h_rez": 2048,  # Horizontal Pixels
+                    "v_rez": 1080,  # Vertical Pixels
+                    "refresh": 59.94,  # Refresh Rate
+                    "tag": "2048x1080 59.94Hz",  # The actual Button text
+                },
+                "2048x1080@50": {
+                    "id": 80,  # command to be called for this resolution
+                    "h_rez": 2048,  # Horizontal Pixels
+                    "v_rez": 1080,  # Vertical Pixels
+                    "refresh": 50,  # Refresh Rate
+                    "tag": "2048x1080 50Hz",  # The actual Button text
+                },
+            },
+        }
+    }
+}
---
+++ request_tests.py
@@ -0,0 +1,31 @@
+import asyncio
+import websockets
+
+
+async def handle_message(websocket):
+    async for message in websocket:
+        print("Received:", message)
+        parts = message.split("||")
+        if len(parts) == 3:
+            # Handle 3-part messages
+            command, value1, value2 = parts
+            print(f"Command: {command}, Value 1: {value1}, Value 2: {value2}")
+
+
+async def send_single_command(websocket, id, value):
+    command = f"\r\nSENDSINGLE||{id},{value}\r\n"
+    print(f"Sending command: {command.strip()}")
+    await websocket.send(command)
+
+
+async def main():
+    uri = "ws://10.0.30.43/ws/uart"
+    async with websockets.connect(uri) as websocket:
+        # Mimic the WebUI command
+        await websocket.send("\r\nSENDSINGLE||97,111\r\n")
+
+        # Start listening to incoming messages
+        await handle_message(websocket)
+
+
+asyncio.get_event_loop().run_until_complete(main())
